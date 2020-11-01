c5cf408630e30cc3ff6956a9291046bf34c11183
Snehil <snehilverma488@gmail.com>
2020-11-01 19:08:45 +0530

    [Snehil] Add : Inserting values into AddressBook

---
+++ UC3-InsertingIntoAddressBook.sql
@@ -0,0 +1,7 @@
+insert into AddressBook values
+('Shreyas','Iyer','1000 Michigan Ave.','Chicago','IL',60605,'3889233120'),
+('Shikhar','Dhawan','1000 Harbor Ave.','Seattle','WA',98116,'8332443923'),
+('Marcus','Stoinis','360 Delmar Blvd.','New York','NY',10011,'8832322103'),
+('Faf','Duplesis','100 5th Ave.','New York','NY',10012,'8043232213'),
+('Johny','Bairstow','Stout St.','Denver','CO',80204,'7884440432'),
+('Jos','Butler','Stout St.','Denver','CO',56478,'9876543217')
